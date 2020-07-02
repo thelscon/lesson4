7f3d9afaa8e3bad363e53bc12fe279ad55e80d50
Igor Dovganyuk <thelscon@gmail.com>
2020-07-02 20:59:04 +0300

    uodate

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+let box = document.querySelector ('.box');
+let moveBy = 200;
+let moved = parseInt (box.style.marginLeft) || 0;
+
+/*do {
+    box.style.marginLeft = ++moved + 'px';
+} while (moved < moveBy - 1);*/
+
+/*while (moved <= moveBy) {
+    box.style.marginLeft = ++moved + 'px';
+}*/
+
+for (let number = 0, isUserHappy = true; isUserHappy; number++) {
+    isUserHappy = confirm ('Are you happy that number is ' + number + ' ?');
+}
